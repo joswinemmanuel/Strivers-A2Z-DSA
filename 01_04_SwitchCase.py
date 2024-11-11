10e15217b7e1f5ed81e7e3ab3401924b88dc6856
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-11-11 21:40:56 +0530

    Switch Case Demo

---
+++ 01_04_SwitchCase.py
@@ -0,0 +1,9 @@
+from math import pi
+
+class Solution:
+    def switchCase(self, choice, arr):
+        # Code here
+        if choice == 1:
+           return(pi * arr[0] * arr[0]);
+        else:
+           return((arr[0] * arr[1]));
