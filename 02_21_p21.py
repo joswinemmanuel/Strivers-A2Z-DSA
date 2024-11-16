bec0ddd22dc9066584f14aa81a0712b8b6c612c1
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-11-16 18:14:57 +0530

    Border

---
+++ 02_21_p21.py
@@ -0,0 +1,9 @@
+def getStarPattern(n: int) -> None:
+    # Write your solution here.
+    for i in range(1, n+1):
+        for j in range(1,n+1):
+            if i==1 or i==n or j==1 or j==n:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
