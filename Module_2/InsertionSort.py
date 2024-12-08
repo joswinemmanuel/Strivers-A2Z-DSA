42d36a48b5cccda631064c85b6a3ea0cfb933fd5
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-12-08 09:01:41 +0530

    Insertion Sort

---
+++ Module_2/InsertionSort.py
@@ -0,0 +1,10 @@
+class Solution:
+    def insertionSort(self, arr):
+        #code here
+        n = len(arr)
+        for i in range(n):
+            j = i
+            while(j>0 and arr[j-1]>arr[j]):
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+                j -= 1
+        return arr
